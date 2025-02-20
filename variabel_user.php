32020780d2e87a82bed38d4d2ea9c7164c8672ca
ciicicuits <sucieramadhanii@gmail.com>
2025-02-20 12:13:59 +0700

    praktikum 1

---
+++ variabel_user.php
@@ -0,0 +1,10 @@
+<?php
+// mendefinisikan variabel 
+$nama = 'Suci Ramadhani';
+$umur = 19;
+$berat = 60;
+
+echo 'Nama saya :' . $nama; 
+echo '<br/>Umur :' . $umur . 'Tahun'; 
+echo '<br/>Berat  :' . $berat . 'Kg';
+?>`
