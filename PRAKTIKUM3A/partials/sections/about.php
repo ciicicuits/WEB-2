5d855e27f550d010f7fd09e39d20fe488e9cf0f2
ciicicuits <sucieramadhanii@gmail.com>
2025-03-12 19:39:42 +0700

    Tugas3_Praktikum

---
+++ PRAKTIKUM3A/partials/sections/about.php
@@ -0,0 +1,24 @@
+<section class="page-section bg-primary text-white mb-0" id="about">
+            <div class="container">
+                <!-- About Section Heading-->
+                <h2 class="page-section-heading text-center text-uppercase text-white">About</h2>
+                <!-- Icon Divider-->
+                <div class="divider-custom divider-light">
+                    <div class="divider-custom-line"></div>
+                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                    <div class="divider-custom-line"></div>
+                </div>
+                <!-- About Section Content-->
+                <div class="row">
+                    <div class="col-lg-4 ms-auto"><p class="lead">A content creator is someone who produces engaging and valuable material for digital platforms, such as social media, blogs, YouTube, podcasts, and websites. Content creators make content in various forms, including videos, articles, infographics, photos, and live streams, often to entertain, educate, or promote a brand.</p></div>
+                    <div class="col-lg-4 me-auto"><p class="lead">A food vlogger is a content creator who focuses on food, restaurants, street food, recipes, and everything related to culinary experiences. They usually create videos reviewing food, exploring culinary destinations, taking on food challenges, or even cooking themselves. Food vlogging is popular on platforms like YouTube, TikTok, Instagram, and Facebook.</p></div>
+                </div>
+                <!-- About Section Button-->
+                <div class="text-center mt-4">
+                    <a class="btn btn-xl btn-outline-light" href="https://startbootstrap.com/theme/freelancer/">
+                        <i class="fas fa-download me-2"></i>
+                        Free Download!
+                    </a>
+                </div>
+            </div>
+        </section>
