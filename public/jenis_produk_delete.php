eff5ca5fc295176efede372cb71870bdb0fa4200
ciicicuits <sucieramadhanii@gmail.com>
2025-05-31 09:54:26 +0700

    praktikum10

--- public/jenis_produk_delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../config/database.php';
-
-$id = $_GET['id'] ?? null;
-$stmt = $pdo->prepare("DELETE FROM jenis_produk WHERE id = ?");
-$stmt->execute([$id]);
-
-header("Location: jenis_produk.php");
-exit;
--- src/views/layouts/footer.php
+++
@@ -1,9 +0,0 @@
-    </div> <!-- end of .row -->
-  </div> <!-- end of .container-fluid -->
-
-  <footer class="text-center mt-5 mb-3">
-    <p>&copy; 2025 Koperasi Pegawai by Suci Ramadhani</p>
-  </footer>
-
-</body>
-</html>
